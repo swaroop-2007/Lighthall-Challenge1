44d461d4892cf52064b4595f56a0f519a00f7504
swaroop-2007 <73997421+swaroop-2007@users.noreply.github.com>
2023-04-14 12:19:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const clicks = parseInt(localStorage.getItem('clicks')) || 0;
+    setCount(clicks);
+  }, []);
+
+  const handleClick = () => {
+    const newCount = count +1;
+    setCount(newCount);
+    localStorage.setItem('clicks', newCount);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div>
+
+        <p> Click Here to Increment the click count!</p>
+
+        <h1>Click Count: {count} </h1>
+        <button onClick={handleClick}>Click ME</button>
+      </div>
   );
 }
 
 export default App;
+
