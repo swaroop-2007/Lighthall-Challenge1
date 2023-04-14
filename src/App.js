014832d56aba7a8a1520bbbd3feb331739129831
swaroop-2007 <73997421+swaroop-2007@users.noreply.github.com>
2023-04-14 12:41:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,30 +4,70 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(parseInt(localStorage.getItem('clicks')) || 0);
+  const [clickLocations, setClickLocations] = useState([]);
 
-  useEffect(() => {
-    const clicks = parseInt(localStorage.getItem('clicks')) || 0;
-    setCount(clicks);
-  }, []);
-
-  const handleClick = () => {
-    const newCount = count +1;
+  const handleClick = (event) => {
+    const newCount = count + 1;
     setCount(newCount);
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        setClickLocations(prevClickLocations => [
+          ...prevClickLocations,
+          {lat: lat, lng: lng, count: newCount}
+        ]);
+      },
+      error => {
+        console.log(error);
+      }
+    );
     localStorage.setItem('clicks', newCount);
   }
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        const clicks = parseInt(localStorage.getItem('clicks')) || 0;
+        setClickLocations(prevClickLocations => [
+          ...prevClickLocations,
+          {lat: lat, lng: lng, count: clicks}
+        ]);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }, []);
+
   return (
-
       <div>
-
-        <p> Click Here to Increment the click count!</p>
-
-        <h1>Click Count: {count} </h1>
+        <p>Click Here to Increment the click count!</p>
+        <h1>Click Count: {count}</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Latitude</th>
+              <th>Longitude</th>
+              <th>Click Count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clickLocations.map((location, index) => (
+              <tr key={index}>
+                <td>{location.lat}</td>
+                <td>{location.lng}</td>
+                <td>{location.count}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <button onClick={handleClick}>Click ME</button>
       </div>
   );
 }
 
 export default App;
-
